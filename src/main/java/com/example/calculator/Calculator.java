555081443e7d80003b203a801e08927e01badc72
minjonyyy <minjonyyy@naver.com>
2025-01-03 15:46:01 +0900

    커밋 테스트2

--- src/main/java/com/example/calculator/Calculator.java
+++ src/main/java/com/example/calculator/Calculator.java
@@ -4,6 +4,6 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
-        System.out.println("커밋테스트");
+        System.out.println("다시 테스트");
     }
 }
