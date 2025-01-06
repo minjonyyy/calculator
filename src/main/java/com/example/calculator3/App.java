b55228cf3612a5148175b208970884edc714a07b
minjonyyy <minjonyyy@naver.com>
2025-01-06 19:28:05 +0900

    LV.3 파일 생성

---
+++ src/main/java/com/example/calculator3/App.java
@@ -0,0 +1,54 @@
+package com.example.calculator3;
+
+import java.util.Objects;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Calculator calculator = new Calculator(); //Calculator 인스턴스 생성
+
+        String stop="";
+        String yesorno="";
+
+        do {
+            System.out.println("-----------------------");
+            System.out.print("첫 번째 숫자를 입력하세요: ");
+            int num1 = sc.nextInt();
+            System.out.print("두 번째 숫자를 입력하세요: ");
+            int num2 = sc.nextInt();
+            sc.nextLine();
+
+            System.out.print("사칙연산 기호를 입력하세요: ");
+            char op = sc.next().charAt(0);
+            sc.nextLine();
+
+            int result = calculator.calculate(num1, num2, op);
+
+            if (result != -1) {
+                System.out.println("결과: " + num1 + " " + op + " " + num2 + " = " + result);
+
+                System.out.println("계산된 결과들:" + calculator.getResults().toString());
+
+            }
+
+            if (!calculator.getResults().isEmpty()){
+                System.out.println("가장 먼저 저장된 데이터 삭제? ('네' 입력 시 삭제)");
+                yesorno = sc.nextLine();
+
+                if (Objects.equals(yesorno, "네")){
+                    calculator.removeResult();
+                    System.out.println("삭제 후 계산된 결과들: "+ calculator.getResults().toString());
+                } else {
+                    System.out.println("결과들 그대로" + calculator.getResults().toString());
+                }
+            }
+
+            System.out.println("더 계산하시겠습니까? (exit 입력 시 종료)");
+            stop= sc.nextLine();
+
+        } while(!stop.equals("exit"));
+
+
+    }
+}
---
+++ src/main/java/com/example/calculator3/Calculator.java
@@ -0,0 +1,63 @@
+package com.example.calculator3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    private List<Integer> results;
+
+    public Calculator() {
+        this.results = new ArrayList<>();
+    }
+
+    public int calculate(int num1, int num2, char op) {
+        int result = 0;
+
+        if (num1 <0 || num2<0){
+            System.out.println("양의 정수(0포함)를 입력해주세요");
+            return -1;
+        }
+
+        switch (op) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                    return -1;
+                } else {
+                    result = num1 / num2;
+                }
+                break;
+            default:
+                System.out.println("잘못된 연산 기호입니다. (+, -, *, / 중 하나를 입력하세요)");
+                return -1;
+        }
+        results.add(result);
+        return result;
+    }
+    
+    //getter 메서드
+    public List<Integer> getResults() {
+        return results;
+    }
+
+    //setter 메서드
+    public void setResults(List<Integer> results) {
+        this.results = results;
+    }
+
+    public void removeResult() {
+        results.remove(0);
+    }
+
+
+}
